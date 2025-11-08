40e013b453a6fa5b53bec825ad2f767a0fb7e9fd
TanHung189 <hung189204@gmail.com>
2025-11-08 22:53:20 +0700

    add UI components such as header, tasklist,... footer

--- frontend/src/components/StatsAndFilters.jsx
+++ frontend/src/components/StatsAndFilters.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { FilterType } from "@/lib/data";
 
-const StatsAndFilters = () => {
-  return <div>StatsAndFilters</div>;
+import React from "react";
+import { Button } from "./ui/button";
+import { Filter } from "lucide-react";
+import { Badge } from "./ui/badge";
+
+const StatsAndFilters = ({
+  completedTaskCount = 0,
+
+  activeTaskCount = 0,
+
+  filter = "all",
+}) => {
+  return (
+    <div className="flex flex-row items-start justify-between gap-4">
+      <div className="flex gap-3">
+        <Badge
+          variant="secondary"
+          className="bg-white text-accent-foreground border-info/20"
+        >
+          {activeTaskCount} {FilterType.active}
+        </Badge>
+
+        <Badge
+          variant="secondary"
+          className="bg-white text-success border-success/20"
+        >
+          {completedTaskCount} {FilterType.completed}
+        </Badge>
+      </div>
+
+      <div className="flex flex-col gap-2 sm:flex-row">
+        {Object.keys(FilterType).map((type) => (
+          <Button
+            key={type}
+            variant={filter === type ? "gradient" : "ghost"}
+            size="sm"
+            className="capitalize"
+          >
+            <Filter className="size-4" />
+            {FilterType[type]}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default StatsAndFilters;
---
+++ frontend/src/components/TaskEmptyState.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TaskEmptyState = ({ filter }) => {
+  return <div>TaskEmptyState</div>;
+};
+
+export default TaskEmptyState;
